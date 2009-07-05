396e1a6ee1b4cec73e48feaf1e93bbdaa06ae016
Michael Gauthier <gauthierm@php.net>
2009-07-05 18:35:21 +0000

    More test coverage for SetAttribute.
    git-svn-id: https://svn.php.net/repository/pear/packages/Services_Amazon_SQS/trunk@283524 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/SetAttributesTestCase.php
+++ tests/SetAttributesTestCase.php
@@ -178,6 +178,94 @@ XML;
     }
 
     // }}}
+    // {{{ testSetAttributeWithInvalidQueue()
+
+    /**
+     * @group attributes
+     * @expectedException Services_Amazon_SQS_InvalidQueueException
+     */
+    public function testSetAttributeWithInvalidQueue()
+    {
+        // {{{ response body
+        $body = <<<XML
+<?xml version="1.0"?>
+<ErrorResponse xmlns="http://queue.amazonaws.com/doc/2009-02-01/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>AWS.SimpleQueueService.NonExistentQueue</Code>
+    <Message>The specified queue does not exist for this wsdl version.</Message>
+    <Detail/>
+  </Error>
+  <RequestId>05714b4b-7359-4527-9bd1-c9aaacb4a2ad</RequestId>
+</ErrorResponse>
+XML;
+
+        $body = $this->formatXml($body);
+        // }}}
+        // {{{ response headers
+        $headers = array(
+            'Content-Type'      => 'text/xml',
+            'Transfer-Encoding' => 'chunked',
+            'Date'              => 'Sun, 18 Jan 2009 17:34:20 GMT',
+            'Cneonction'        => 'close', // Intentional misspelling
+            'Server'            => 'AWS Simple Queue Service'
+        );
+        // }}}
+        $this->addHttpResponse($body, $headers, 'HTTP/1.1 400 Bad Request');
+
+        $queue = new Services_Amazon_SQS_Queue(
+            'http://queue.amazonaws.com/this-queue-does-not-exist',
+            '123456789ABCDEFGHIJK',
+            'abcdefghijklmnopqrstuzwxyz/ABCDEFGHIJKLM',
+            $this->request
+        );
+
+        $queue->setAttribute('VisibilityTimeout', 10000);
+    }
+
+    // }}}
+    // {{{ testSetAttributeWithUnknownError()
+
+    /**
+     * @group attributes
+     * @expectedException Services_Amazon_SQS_ErrorException
+     */
+    public function testSetAttributeWithUnknownError()
+    {
+        // {{{ response body
+        $body = <<<XML
+<?xml version="1.0"?>
+<ErrorResponse xmlns=" http://queue.amazonaws.com/doc/2009-02-01/">
+  <Error>
+    <Type>Receiver</Type>
+    <Code>InternalError</Code>
+    <Message>We encountered an internal error. Please try again.</Message>
+    <Detail/>
+  </Error>
+  <RequestId>d22acfe7-a4c3-4ab4-b0f3-3fbc20b97c1d</RequestId>
+</ErrorResponse>
+XML;
+
+        $body = $this->formatXml($body);
+        // }}}
+        // {{{ response headers
+        $headers = array(
+            'Transfer-Encoding' => 'chunked',
+            'Date'              => 'Sun, 18 Jan 2009 17:34:20 GMT',
+            'Cneonction'        => 'close', // Intentional misspelling
+            'Server'            => 'AWS Simple Queue Service'
+        );
+        // }}}
+        $this->addHttpResponse(
+            $body,
+            $headers,
+            'HTTP/1.1 500 Internal Server Error'
+        );
+
+        $this->queue->setAttribute('VisibilityTimeout', 10000);
+    }
+
+    // }}}
 }
 
 ?>
