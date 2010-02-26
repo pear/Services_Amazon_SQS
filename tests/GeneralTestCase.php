5b61ffb2499bff178904b0bd9afa9aadaae08948
Michael Gauthier <gauthierm@php.net>
2010-02-26 23:39:05 +0000

    Add unit test class for general tests.
    git-svn-id: https://svn.php.net/repository/pear/packages/Services_Amazon_SQS/trunk@295547 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/GeneralTestCase.php
@@ -0,0 +1,83 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * General tests for the Services_Amazon_SQS package.
+ *
+ * These tests require the PHPUnit3 package to be installed. PHPUnit is
+ * installable using PEAR. See the
+ * {@link http://www.phpunit.de/pocket_guide/3.3/en/installation.html manual}
+ * for detailed installation instructions.
+ *
+ * This test suite follows the PEAR AllTests conventions as documented at
+ * {@link http://cvs.php.net/viewvc.cgi/pear/AllTests.php?view=markup}.
+ *
+ * LICENSE:
+ *
+ * Copyright 2009 silverorange
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @category  Services
+ * @package   Services_Amazon_SQS
+ * @author    Mike Brittain <mike@mikebrittain.com>
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2009 silverorange
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/Services_Amazon_SQS
+ * @link      http://aws.amazon.com/sqs/
+ * @link      http://s3.amazonaws.com/awsdocs/SQS/20080101/sqs-dg-20080101.pdf
+ */
+
+/**
+ * PHPUnit3 framework
+ */
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * Services_Amazon_SQS test base class
+ */
+require_once dirname(__FILE__) . '/TestCase.php';
+
+/**
+ * Constructor tests for Services_Amazon_SQS
+ *
+ * @category  Services
+ * @package   Services_Amazon_SQS
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2009 silverorange
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      http://pear.php.net/package/Services_Amazon_SQS
+ * @link      http://aws.amazon.com/sqs/
+ * @link      http://s3.amazonaws.com/awsdocs/SQS/20080101/sqs-dg-20080101.pdf
+ */
+class Services_Amazon_SQS_GeneralTestCase extends
+    Services_Amazon_SQS_TestCase
+{
+    // {{{ testSetMaximumRetries()
+
+    /**
+     * @group general
+     */
+    public function testSetMaximumRetries()
+    {
+        $this->queue->setMaximumRetries(25);
+        $this->assertAttributeEquals(25, 'maximumRetries', $this->queue);
+    }
+
+    // }}}
+}
+
+?>
